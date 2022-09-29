341321d409dfd2a75f6d61317110e12e38d0939d
xxanonx <semorebutts537@gmail.com>
2022-09-29 00:35:30 -0400

    improvements to generational stuff, making of model and running. just need to be able to fit and get predictions

--- 3d_tic_tac_toe/3d_ttt.py
+++ 3d_tic_tac_toe/3d_ttt.py
@@ -17,11 +17,11 @@ session = tf.compat.v1.InteractiveSession(config=config)"""
 
 IN_A_ROW = 3
 EMPTY_BOARD = np.zeros((IN_A_ROW, IN_A_ROW, IN_A_ROW), dtype='int8')
+EMPTY_BOARD_MOVES = np.zeros((IN_A_ROW, IN_A_ROW), dtype='float16')
 ONES = np.ones((IN_A_ROW, IN_A_ROW, IN_A_ROW), dtype='int8')
 NEG_ONES = (np.ones((IN_A_ROW, IN_A_ROW, IN_A_ROW), dtype='int8') * -1)
 
 random.seed(time.time_ns())
-games_played = 0
 actual_play = False
 
 # dna related stuff
@@ -43,11 +43,14 @@ class BoardEnv:
 
     def __init__(self):
         self.board = np.copy(EMPTY_BOARD)
-        self.previous_board_O = np.copy(self.board)
-        self.previous_board_X = np.copy(self.board)
+        self.board_history_O = []
+        self.board_history_X = []
+        self.move_history_O = []
+        self.move_history_X = []
         self.current_board = np.copy(self.board)
         self.game_over = False
         self.whose_turn = True
+        self.winner = 0
         # -1 = O and 1 = X
         # false = 0 and true = X
 
@@ -67,8 +70,10 @@ class BoardEnv:
             [0. 0. 0.]
             [0. 0. 0.]]]"""
         self.board = np.copy(EMPTY_BOARD)
-        self.previous_board_O = np.copy(self.board)
-        self.previous_board_X = np.copy(self.board)
+        self.board_history_O = []
+        self.board_history_X = []
+        self.move_history_O = []
+        self.move_history_X = []
         self.current_board = np.copy(self.board)
 
         self.game_over = False
@@ -107,30 +112,39 @@ class BoardEnv:
             winner = 0
         return self.game_over, winner
 
-    def make_move(self, x: int, y: int, just_data=False):
+    def make_move(self, x: int, y: int, actor_answer=None):
         move_made: bool = False
+        actor_decides = actor_answer is not None
         self.current_board = np.copy(self.board)
+
+        if actor_decides:
+            armax = tf.argmax(actor_answer).numpy()
+            x = armax.argmax()
+            y = armax.max()
+
         layer_num = 0
         for layer in self.board:
             if move_made:
                 break
             if layer[y][x] == 0:
-                if not just_data:
-                    move2 = np.copy(EMPTY_BOARD)
-                    move2[y][x] = 1
-                    if self.whose_turn:
-                        self.board[layer_num][y][x] = 1
-                        self.previous_board_X = np.copy(self.board)
-                        move_made = True
-                    else:
-                        self.board[layer_num][y][x] = -1
-                        self.previous_board_O = np.copy(self.board)
-                        move_made = True
+                # move2 is used for move data
+                move2 = np.copy(EMPTY_BOARD_MOVES)
+                move2[y][x] = 1
+                if self.whose_turn:
+                    self.board[layer_num][y][x] = 1
+                    self.board_history_X.append(np.copy(self.board))
+                    self.move_history_X.append(np.copy(move2))
+                    move_made = True
+                else:
+                    self.board[layer_num][y][x] = -1
+                    self.board_history_O.append(np.copy(self.board))
+                    self.move_history_O.append(np.copy(move2))
+                    move_made = True
             layer_num += 1
         # return false if the move can't be made
         return move_made
 
-    def get_state(self, human=False, random_play=False, verbose=False):
+    def get_state(self, human=False, random_play=False, verbose=False, actor_answer=None):
         # Whose turn matters and human matters
         input_max = (IN_A_ROW - 1)
         while True:
@@ -183,25 +197,27 @@ class BoardEnv:
                     final_move[0] = random.randint(0, input_max)
                     final_move[1] = random.randint(0, input_max)
 
-            if self.make_move(final_move[0], final_move[1]):
+            if self.make_move(final_move[0], final_move[1], actor_answer):
                 if verbose:
                     print(f"{final_move} is acceptable")
                 break
             else:
+                if not random_play and actor_answer is not None:
+                    armax = tf.argmax(actor_answer).numpy()
+                    actor_answer[armax.max()][armax.argmax()] = -1
                 if verbose:
                     print(f"it seems that {final_move} is not legal")
 
-        done, winner_ = self.look_for_win()
+        done, self.winner =self.look_for_win()
         if done:
             if verbose:
                 print(self.board)
-                if winner_ == -1:
+                if self.winner == -1:
                     print("O's WON!!!")
-                elif winner_ == 1:
+                elif self.winner == 1:
                     print("X's WON!!!!")
                 else:
                     print("DRAW")
-
             # self.reset()
 
         # end of turn
@@ -216,6 +232,9 @@ class Player:
         self.do_not_init_a = False
         self.dna = []
         self.loss = ""
+        if not self.do_not_init_a:
+            self.init_actor()
+            self.do_not_init_a = True
 
     def init_actor(self):
         # Actor trained on wins
@@ -238,13 +257,13 @@ class Player:
 
         self.make_actor()
 
-    def mutate_actor(self):
+    def mutate_actor(self, override_max_number_of_mutations=4):
         del self.Actor_model
         self.Actor_model = Sequential()
         max_size = len(self.dna)
         what_layers_mutated = []
         # how many mutations
-        for mutation in range(random.randint(1, 4)):
+        for mutation in range(random.randint(1, override_max_number_of_mutations)):
             where = random.randint(1, (max_size - 2))
             type_of_mutation = random.randint(0, 3)
             if type_of_mutation == 0:
@@ -269,7 +288,7 @@ class Player:
 
     def make_actor(self):
         number_of_layers = len(self.dna)
-        self.Actor_model.add(Dense(self.dna[0].size, input_shape=(4, IN_A_ROW, IN_A_ROW, IN_A_ROW),
+        self.Actor_model.add(Dense(self.dna[0].size, input_shape=(IN_A_ROW, IN_A_ROW, IN_A_ROW),
                                    activation=self.dna[0].activation))
         strand_num = 0
         for strand in self.dna:
@@ -279,6 +298,7 @@ class Player:
             if strand_num == int(number_of_layers / 2):
                 # halfway through flatten
                 self.Actor_model.add(Flatten())
+            strand_num += 1
 
         self.Actor_model.add(Reshape((3, 3)))
         self.Actor_model.compile(
@@ -287,11 +307,9 @@ class Player:
             metrics=['accuracy']
         )
         self.Actor_model.summary()
+        # self.Actor_model.fit(x=np.array([np.copy(EMPTY_BOARD)]))
 
     def teach_actor(self, states, moves):
-        if not self.do_not_init_a:
-            self.init_actor()
-            self.do_not_init_a = True
         div = int((len(states) * 0.7))
         train_x = np.copy(states[:div]).astype('float16')
         validation_x = np.copy(states[div:]).astype('float16')
@@ -398,30 +416,68 @@ def visualize3d(board3d):
     plt.show()
 
 
+def reward_previous_moves(moves, multiplier):
+    moves.reverse()
+    for move in moves:
+        move *= multiplier
+        multiplier /= 2
+    moves.reverse()
+    return moves.copy()
+
+
+list_of_games = []
+list_of_moves = []
 list_of_boards = []
+randomness = 1.0
 for i in range(500):
     list_of_boards.append(BoardEnv())
 
-avg = 0
-actual_play = True
-total = time.perf_counter()
-prog_bar = tqdm(total=2000)
-while games_played < 2000:
-    for board in list_of_boards:
-        start = time.perf_counter()
-        board.get_state(False, True)
-        if board.game_over:
-            end = time.perf_counter()
-            avg += ((end - start)*1000)
-            # print(str((end - start)*1000) + " milliseconds")
-            # board.vis()
-            board.reset()
-            prog_bar.update(1)
-            if games_played >= 2000:
-                break
+actor = Player()
 
+actual_play = True
+first_run = True
+set_amount_of_games = 10000
+total = time.perf_counter()
+for gen in range(20):
+    games_played = 0
+    prog_bar = tqdm(total=set_amount_of_games)
+    while games_played < set_amount_of_games:
+        for board in list_of_boards:
+            chance_of_random = random.randint(0, int(randomness * 100))
+            if chance_of_random != 0 or first_run:
+                board.get_state(False, True)
+            else:
+                move = actor.Actor_model(np.copy(board.board))
+                board.get_state(False, False, actor_answer=move)
+            if board.game_over:
+                temp_moves_X = []
+                temp_moves_O = []
+                if board.winner == -1:
+                    temp_moves_O = reward_previous_moves(board.move_history_O, 1)
+                    temp_moves_X = reward_previous_moves(board.move_history_X, -1)
+                elif board.winner == 1:
+                    temp_moves_O = reward_previous_moves(board.move_history_O, -1)
+                    temp_moves_X = reward_previous_moves(board.move_history_X, 1)
+                else:
+                    temp_moves_O = reward_previous_moves(board.move_history_O, -1)
+                    temp_moves_X = reward_previous_moves(board.move_history_X, -1)
+                for i in board.board_history_O:
+                    i *= -1
+                list_of_moves.extend(temp_moves_X)
+                list_of_moves.extend(temp_moves_O)
+                list_of_games.extend(board.board_history_X.copy())
+                list_of_games.extend(board.board_history_O.copy())
+                board.reset()
+                prog_bar.update(1)
+                if games_played >= set_amount_of_games:
+                    break
+    if first_run:
+        first_run = False
+    actor.teach_actor(np.array(list_of_games), np.array(list_of_moves))
+    randomness *= 0.95
 time.sleep(0.1)
-print("total: " + str(time.perf_counter() - total))
-avg /= games_played
-print("per game: " + str(avg) + " milliseconds")
-print(games_played)
+end = (time.perf_counter() - total)
+print("total: " + str(end))
+end /= (games_played * 20)
+print("per game: " + str(end) + " milliseconds")
+print(games_played * 20)
